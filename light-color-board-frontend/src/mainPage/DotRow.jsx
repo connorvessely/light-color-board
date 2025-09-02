ea6d8b78266faf0809b0499effe8f380a8c4b192
Connor Vessely <connorvessely@gmail.com>
2025-09-02 12:13:23 -0400

    Clean code

--- light-color-board-frontend/src/mainPage/DotRow.jsx
+++ light-color-board-frontend/src/mainPage/DotRow.jsx
@@ -2,7 +2,7 @@ import ColorDot from "./ColorDot"
 function DotRow(props){
     const rows = Array.from({ length: props.length }, (_, i) => i)
     return (
-        <div className="dot-row">
+        <div className={"dot-row " + props.rowIndex}>
             {rows.map(i => <ColorDot className = {i + " " + props.rowIndex} key={i + " " + props.rowIndex} /> )}
         </div>
     )
