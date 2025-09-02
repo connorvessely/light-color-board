78fe0ddc42edeb92419b23108c2cd4f09e2a76c0
Connor Vessely <connorvessely@gmail.com>
2025-09-02 12:29:43 -0400

    Offset even dotrows

--- light-color-board-frontend/src/mainPage/DotRow.jsx
+++ light-color-board-frontend/src/mainPage/DotRow.jsx
@@ -1,9 +1,9 @@
 import ColorDot from "./ColorDot"
-function DotRow(props){
-    const rows = Array.from({ length: props.length }, (_, i) => i)
+function DotRow({rowIndex, length}){
+    const rows = Array.from({ length: length }, (_, i) => i)
     return (
-        <div className={"dot-row " + props.rowIndex}>
-            {rows.map(i => <ColorDot className = {i + " " + props.rowIndex} key={i + " " + props.rowIndex} /> )}
+        <div className={"dot-row " + (rowIndex%2 == 0 ? "even" : "")}>
+            {rows.map(i => <ColorDot className = {i + " " + rowIndex} key={i + " " + rowIndex} /> )}
         </div>
     )
 }
